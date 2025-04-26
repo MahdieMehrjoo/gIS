2148728bf01e448cccb0fceb0236ba31aa3a68a9
Sudabe <soodabehan@gmail.com>
2025-04-26 04:28:55 +0330

    book1

--- Book.java
+++ Book.java
@@ -1,11 +1,34 @@
+package common;
+
 public class Book {
-    // کدهای مربوط به کلاس Book
-}
+    private String id;
+    private String title;
+    private String author;
+    private String publicationDate;
+    private String isbn;
+    private int copies;
+    private String publisher;
 
-class Author {
-    // کدهای مربوط به کلاس Author
-}
+    public Book(String id, String title, String author, String publicationDate, String isbn, int copies, String publisher) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publicationDate = publicationDate;
+        this.isbn = isbn;
+        this.copies = copies;
+        this.publisher = publisher;
+    }
 
-class Publisher {
-    // کدهای مربوط به کلاس Publisher
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getPublicationDate() { return publicationDate; }
+    public String getIsbn() { return isbn; }
+    public int getCopies() { return copies; }
+    public String getPublisher() { return publisher; }
+
+    @Override
+    public String toString() {
+        return id + "," + title + "," + author + "," + publicationDate + "," + isbn + "," + copies + "," + publisher;
+    }
 }
